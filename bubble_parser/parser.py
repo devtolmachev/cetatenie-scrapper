b8c865bbd1aab06256fed5faf38f197a49f408b2
devtolmachev <dev.tolmachev@gmail.com>
2024-05-28 09:26:01 +0300

    improved parser

--- bubble_parser/parser.py
+++ bubble_parser/parser.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import asyncio
 import datetime
+import json
 import random
 import re
 import shutil
@@ -47,7 +48,8 @@ def aiohttp_session(
         async def inner(self: ParserCetatenie, *args, **kwargs):
             nonlocal attempts
 
-            headers = {"User-Agent": UserAgent().random, "Accept": "*/*"}
+            headers = HEADERS
+            headers["User-Agent"] = UserAgent().random
             client_timeout = ClientTimeout(total=timeout)
             connector = TCPConnector(ssl=False, limit_per_host=10)
             async with ClientSession(
@@ -75,6 +77,43 @@ def aiohttp_session(
     return wrapper
 
 
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,my;q=0.6",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Pragma": "no-cache",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "sec-ch-ua": '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Linux"',
+}
+
+
+PDF_HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,my;q=0.6",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Pragma": "no-cache",
+    "Referer": "https://cetatenie.just.ro/ordine-articolul-1-1/",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "sec-ch-ua": '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Linux"',
+}
+
+
 class ParserCetatenie:
     """Class which provide web scrapping methods."""
 
@@ -83,7 +122,7 @@ class ParserCetatenie:
         "11": "https://cetatenie.just.ro/ordine-articolul-1-1/",
     }
 
-    @aiohttp_session(sleeps=(4, 10), attempts=10, timeout=6)
+    @aiohttp_session(sleeps=(4, 10), attempts=10, timeout=3)
     async def parse_articoluls(  # noqa: C901, PLR0912
         self, session: ClientSession, articoluls_data: dict, path_data: str
     ) -> dict[str, list[dict]]:
@@ -139,6 +178,17 @@ class ParserCetatenie:
         TypeError
             if articoluls_data keys don't validated.
         """
+        # return {"articolul_10": [
+        #     {
+        #         "list_name": "565P",
+        #         "number_order": "12121/34534",
+        #         "year": "2022",
+        #         "date": "18.07.20022",
+        #         "pdf_link": "hjgjhghjghj",
+        #         "timestamp": time.time(),
+        #     }
+        # ]
+        # }
         tasks = []
         # Create path for PDF's
         Path(path_data).mkdir(parents=True, exist_ok=True)
@@ -155,6 +205,11 @@ class ParserCetatenie:
             articolul_num = int(articolul.split("_")[-1])
             # Url for needed article
             url = self.articolul_urls[str(articolul_num)]
+
+            # Set headers
+            headers = HEADERS
+            headers["User-Agent"] = session.headers["User-Agent"]
+            session._default_headers = headers
 
             async with session.get(url) as resp:
                 page_text = await resp.text()
@@ -217,7 +272,7 @@ class ParserCetatenie:
             num: int,
             url_pdf: str,
         ):
-            numbers = await pdf.extract_numbers(path)
+            numbers = await asyncio.to_thread(pdf.extract_numbers, path)
 
             for number_order in numbers:
                 articoluls[f"articolul_{articolul_num}"].append(
@@ -227,7 +282,7 @@ class ParserCetatenie:
                         "year": dt.year,
                         "date": dt.strftime("%d.%m.%Y"),
                         "pdf_link": url_pdf,
-                        "timestamp": time.time()
+                        "timestamp": time.time(),
                     }
                 )
 
@@ -257,30 +312,39 @@ class ParserCetatenie:
         shutil.rmtree(path_data)
         return articoluls
 
-    @aiohttp_session(sleeps=(4, 10), attempts=7)
     async def _collect_data(
         self,
-        session: ClientSession,
         url: str,
         dt: datetime.datetime,
         articolul_num: int,
         num: int,
         src_path: str,
     ) -> tuple[str, datetime.datetime, int]:
-        async with session.get(url) as resp:
-            resp.raise_for_status()
+        @aiohttp_session(sleeps=(5, 10), attempts=10)
+        async def parse(self, session: ClientSession):
+            # Set headers
+            headers = PDF_HEADERS
+            headers["User-Agent"] = session.headers["User-Agent"]
+            session._default_headers = headers
 
-            fn = f"{src_path}/{url.split("/")[-1]}"
-            async with aiofiles.open(fn, "wb") as f:
-                await f.write(await resp.read())
+            await asyncio.sleep(random.uniform(3, 7))
+
+            async with session.get(url) as resp:
+                resp.raise_for_status()
+                fn = str(Path(src_path) / f"{url.split("/")[-1]}")
+
+                async with aiofiles.open(fn, "wb") as f:
+                    await f.write(await resp.read())
 
             return fn, dt, articolul_num, num, url
+
+        return await parse(self)
 
 
 class ParserPDF:
     """Class which provide methods for parsing pdf."""
 
-    async def extract_numbers(self, path_to_pdf: str | Path) -> list[str]:
+    def extract_numbers(self, path_to_pdf: str | Path) -> list[str]:
         """Extract numbers from pdf which downloaded from cetatenie.just.ro."""
         if not Path(path_to_pdf).exists():
             raise FileNotFoundError(path_to_pdf)
@@ -293,3 +357,17 @@ class ParserPDF:
                 None, [re.match(r"(\d+\.) (.+)", string=line) for line in lines]
             )
         ]
+
+
+async def main():
+    p = ParserCetatenie()
+    data = {"articolul_10": [], "articolul_11": []}
+    src_path = f"{time.time()}_{len(str(data))}"
+    result = await p.parse_articoluls(data, src_path)
+    
+    with open("result.json", "w") as f:
+        f.write(json.dumps(result, indent=2))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- tests/server_receiver.py
+++ tests/server_receiver.py
@@ -1,6 +1,5 @@
 """Test receiver server."""
 
-import json
 
 from fastapi import FastAPI, Request
 
@@ -11,9 +10,10 @@ app = FastAPI()
 async def handle_post_request(request: Request) -> None:
     """Handle wehook response."""
     data = await request.json()
+    print(data)
 
-    with open("res.json", "w") as f:
-        f.write(json.dumps(data))
+    # with open("res.json", "w") as f:
+    #     f.write(json.dumps(data))
 
     assert isinstance(data, dict)
 
