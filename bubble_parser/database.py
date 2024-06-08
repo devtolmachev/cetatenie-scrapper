4598af684fee88e7b711932dce96ddf727662f39
devtolmachev <dev.tolmachev@gmail.com>
2024-06-08 13:37:13 +0300

    issolation level autocommit

--- bubble_parser/database.py
+++ bubble_parser/database.py
@@ -31,7 +31,9 @@ def create_sqlalchemy_async_engine() -> AsyncEngine:
         f"postgresql+asyncpg://{cfg["user"]}:{cfg["password"]}@"
         f"{cfg["host"]}:{cfg["port"]}/{cfg["dbname"]}"
     )
-    return create_async_engine(url).execution_options(autocommit=True)
+    return create_async_engine(url).execution_options(
+        isolation_level="AUTOCOMMIT"
+    )
 
 
 async def write_result(articolul_num: int, pdfs: list[dict]) -> None:
